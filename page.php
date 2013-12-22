3813e2fd774e4ecb13d35a49f3a8b95ad91d8d6d
Thomas ZILLIOX <thomas@zilliox.me>
2013-12-22 16:54:25 +0100

    Add Page bandeau

--- page.php
+++ page.php
@@ -1,23 +1,25 @@
-<?php
-  
-
-get_header(); ?>
+<?php get_header(); ?>
 <?php the_post(); ?>
-<section class=" content mod item full-width ">
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'themename' ), 'after' => '</div>' ) ); ?>
-						<?php edit_post_link( __( 'Edit', 'themename' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-<?php the_ID(); ?> -->
-
-				<?php // comments_template( '', true ); ?>
-</section>
+    <section class="content">
+        <header class="content-bandeau">
+            <img class="content-bandeauImage" src="<?php echo get_template_directory_uri(); ?>/images/bandeau-atelier-des-medias.png"/>
+        </header>
+        
+        <aside class="content-aside">
+        </aside>
+        
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header>
+            
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <?php wp_link_pages(array('before' => '<div class="page-link">' . __('Pages:', 'themename'), 'after' => '</div>')); ?>
+                <?php edit_post_link(__('Edit', 'themename'), '<span class="edit-link">', '</span>'); ?>
+            </div>
+        </article>
+    </section>
 
 <?php get_footer(); ?>
