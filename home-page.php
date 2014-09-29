f45f6b7f3a1c121b877996e0cba518706073435a
Martin Delille <martin@phonations.com>
2014-09-29 16:31:24 +0200

    better side by side

--- home-page.php
+++ home-page.php
@@ -9,38 +9,31 @@ get_header(); ?>
 
 <?php the_post(); ?>
 
-     <section class="home-bandeau">
-        <header class="content-bandeau">
-            <img class="content-bandeauImage" src="<?php echo get_template_directory_uri(); ?>/images/bandeau-atelier-des-medias.png"/>
-        </header>
-     </section>
+    <img class="content-bandeauImage" src="<?php echo get_template_directory_uri(); ?>/images/bandeau-atelier-des-medias.png"/>
 
-     <section class="home-map-container">
-        <div id="map_canvas"/>
-        <script type="text/javascript">
-            function initialize() {
-            var latlng = new google.maps.LatLng(-34.397, 150.644);
-            var myOptions = {
-                zoom: 8,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById("map_canvas"),
-              myOptions);
-            }
-            google.maps.event.addDomListener(window, "load", initialize);
+    <div id="home_map_and_text">
+        <div id="home_map_container">
+            <div id="home_map_canvas"></div>
+       </div>
 
-        </script>
-    </section>
-
-   <section class="content">
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
-            <div class="entry-content">
-                <?php the_content(); ?>
-                <?php wp_link_pages(array('before' => '<div class="page-link">Pages : ', 'after' => '</div>')); ?>
-                <?php edit_post_link('Editer', '<span class="edit-link">', '</span>'); ?>
-            </div>
-        </article>
-    </section>
-
+        <div id="home_text">
+            <?php the_content(); ?>
+            <?php wp_link_pages(array('before' => '<div class="page-link">Pages : ', 'after' => '</div>')); ?>
+            <?php edit_post_link('Editer', '<span class="edit-link">', '</span>'); ?>
+        </div>
+    </div>
+            <script type="text/javascript">
+                function initialize() {
+                    var latlng = new google.maps.LatLng(-34.397, 150.644);
+                    var myOptions = {
+                        zoom: 8,
+                        center: latlng,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    };
+                    var map = new google.maps.Map(document.getElementById("home_map_canvas"),
+                      myOptions);
+                }
+                google.maps.event.addDomListener(window, "load", initialize);
+            </script>
+ 
 <?php get_footer(); ?>
