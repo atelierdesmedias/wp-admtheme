c4164d13ead003f652b0f4c98980817f28adc029
Martin Delille <martin@phonations.com>
2014-09-29 15:34:26 +0200

    put bandeau on top section

--- home-page.php
+++ home-page.php
@@ -9,6 +9,12 @@ get_header(); ?>
 
 <?php the_post(); ?>
 
+     <section class="home-bandeau">
+        <header class="content-bandeau">
+            <img class="content-bandeauImage" src="<?php echo get_template_directory_uri(); ?>/images/bandeau-atelier-des-medias.png"/>
+        </header>
+     </section>
+
      <section class="home-map-container">
         <div id="map_canvas"/>
         <script type="text/javascript">
@@ -28,18 +34,11 @@ get_header(); ?>
     </section>
 
    <section class="content">
-        <header class="content-bandeau">
-            <img class="content-bandeauImage" src="<?php echo get_template_directory_uri(); ?>/images/bandeau-atelier-des-medias.png"/>
-        </header>
-        
-        <aside class="content-aside">
-        </aside>
-        
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
             <header class="entry-header">
                 <h1 class="entry-title"><?php the_title(); ?></h1>
             </header>
-            
+
             <div class="entry-content">
                 <?php the_content(); ?>
                 <?php wp_link_pages(array('before' => '<div class="page-link">Pages : ', 'after' => '</div>')); ?>
