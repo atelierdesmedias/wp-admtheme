1b5d0b2ceccdaa574972a8e7d490b6c998ea9f90
Thibault Clauzier <tclauzier@gmail.com>
2014-09-29 18:19:37 +0200

    home page coworkers

--- home-page.php
+++ home-page.php
@@ -6,7 +6,7 @@
  *
  */
 
-<?php get_header(); ?>
+get_header(); ?>
 <?php the_post(); ?>
 
     <section class="content">
@@ -29,5 +29,191 @@
             </div>
         </article>
     </section>
+
+
+
+
+
+
+<section id="home-coworkers">
+    <ul id="home-coworkers-filterlist">
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Les coworkers</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Graphisme</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Web</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Pige</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Rédaction</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Photographie</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Traduction</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Tourisme</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Communication</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Graphisme</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Web</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Pige</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Rédaction</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Photographie</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Traduction</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Tourisme</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Communication</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Graphisme</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Web</a></li>
+        <li class="home-coworkers-filterlist-item"><a href="javascript: void(0);">Pige</a></li>
+    </ul>
+
+    <ul id="home-coworkers-list">
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker1.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Thomas</span>
+                    <span class="coworker-lastname">Zilliox</span>
+                    <span class="coworker-job">Développeur</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker2.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Amélie</span>
+                    <span class="coworker-lastname">Morin-Fontaine</span>
+                    <span class="coworker-job">Edition livres numériques</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker3.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Pierre</span>
+                    <span class="coworker-lastname">Goyou-Beauchamps</span>
+                    <span class="coworker-job">Traducteur</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker4.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Philippe</span>
+                    <span class="coworker-lastname">Vouillon</span>
+                    <span class="coworker-job">Journaliste</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker5.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Laurent</span>
+                    <span class="coworker-lastname">Victorino</span>
+                    <span class="coworker-job">Développeur Jeux vidéos</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker1.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Thomas</span>
+                    <span class="coworker-lastname">Zilliox</span>
+                    <span class="coworker-job">Développeur</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker2.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Amélie</span>
+                    <span class="coworker-lastname">Morin-Fontaine</span>
+                    <span class="coworker-job">Edition livres numériques</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker3.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Pierre</span>
+                    <span class="coworker-lastname">Goyou-Beauchamps</span>
+                    <span class="coworker-job">Traducteur</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker4.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Philippe</span>
+                    <span class="coworker-lastname">Vouillon</span>
+                    <span class="coworker-job">Journaliste</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+        <li class="home-coworkers-list-item">
+            <a href="javascript: void(0);">
+                <img src="<?= get_bloginfo('template_url'); ?>/images/coworkers/coworker5.png" />
+                <div class="coworker-meta">
+                    <span class="coworker-firstname">Laurent</span>
+                    <span class="coworker-lastname">Victorino</span>
+                    <span class="coworker-job">Développeur Jeux vidéos</span>
+                </div>
+                <div class="coworker-rollover">
+                    <img class="picto" src="<?= get_bloginfo('template_url'); ?>/images/picto-rollover.png" alt=""/>
+                    <span class="citation">"Lorem ipsum et blablabdsfz dfg atern iol de tues avec de petits pois dans la cuisine"</span>
+                    <span class="desc">Elu coworker fou par dénonciation le 10/02/2014</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+</section>
+
+
 
 <?php get_footer(); ?>
