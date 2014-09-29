153ae19806e58ea0d2914f5a76d750d076ee6ad1
Martin Delille <martin@phonations.com>
2014-09-29 16:01:09 +0200

    remove page title

--- home-page.php
+++ home-page.php
@@ -35,10 +35,6 @@ get_header(); ?>
 
    <section class="content">
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
-            <header class="entry-header">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-            </header>
-
             <div class="entry-content">
                 <?php the_content(); ?>
                 <?php wp_link_pages(array('before' => '<div class="page-link">Pages : ', 'after' => '</div>')); ?>
