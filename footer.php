11eeacb55dc8e390c421f9cd5f6b0d935db99059
Martin Delille <martin@phonations.com>
2014-09-30 16:24:43 +0200

    fix credit link

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
    <tr>
        <td><img src="<?php echo get_template_directory_uri(); ?>/images/logo-footer.png"></img></td>
       <td id="footer-text"><p>9 quai André Lassagne - 69001 Lyon</p><p style="color:#848484">Tel : 09 72 33 20 92</p></td>
-      <td id="footer-links"><a href="http://atelier-medias.org/credit">Crédit</a> - <a href="http://atelier-medias.org/licence">Licence</a>	</td>
+      <td id="footer-links"><a href="<?php echo home_url('/credits'); ?>">Crédits</a> - <a href="<?php echo home_url('/licence'); ?>">Licence</a>	</td>
       </tr>
       </table>
   </div>
