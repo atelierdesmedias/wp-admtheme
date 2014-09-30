0a2d7c83de08898921597bb8d533de52bd4c727b
tmortagne <thomas.mortagne@gmail.com>
2014-09-30 15:40:33 +0200

    Licensing process proposal

--- 404.php
+++ 404.php
@@ -1,7 +1,28 @@
 <?php
-  
 
-get_header(); ?>
+/**
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This file is part of wp-admtheme.
+ *
+ * wp-admtheme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wp-admtheme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wp-admtheme.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
+
+<?php get_header(); ?>
 
 <article id="post-0" class="post error404 not-found" role="article">
 				<header class="entry-header">
