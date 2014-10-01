5503eb18ae9d87e37de0256de98d00f5c3df96e3
Martin Delille <martin@phonations.com>
2014-10-01 12:02:53 +0200

    blog, categoy and author post list have same layout

--- category.php
+++ category.php
@@ -3,17 +3,19 @@
 
 get_header(); ?>
 
-<header class="page-header">
-					<h1 class="page-title"><?php
-						printf( __( 'Category Archives: %s', 'themename' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-					?></h1>
+    <header class="page-header">
+        <h1 class="entry-title"><?php
+						printf( __( 'Catégorie: %s', 'themename' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+        ?></h1>
 
 					<?php $categorydesc = category_description(); if ( ! empty( $categorydesc ) ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . $categorydesc . '</div>' ); ?>
-				</header>
+    </header>
 
-				<?php get_template_part( 'loop', 'category' ); ?>
+    <div class="blog">
+      <?php get_template_part( 'loop', 'category' ); ?>
+    </div>
 
 </section>
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
