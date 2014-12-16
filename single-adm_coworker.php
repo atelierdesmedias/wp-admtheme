2f13f1955a8f4f9730c487264a4237d500db5b90
Thibault Clauzier <tclauzier@gmail.com>
2014-12-16 09:57:24 +0100

    display social links on coworker page

--- single-adm_coworker.php
+++ single-adm_coworker.php
@@ -15,7 +15,29 @@
                 <div class="coworker-company"><?= $coworker->get('metier'); ?></div>
             </div>
             <div class="coworker-social">
-                Facebook, Twitter, etc.
+                <?php
+                $social_links = array(
+                    'facebook' => $coworker->get('facebook'),
+                    'twitter' => $coworker->get('twitter'),
+                    'linked_in' => $coworker->get('linked_in'),
+                    'viadeo' => $coworker->get('viadeo'),
+                    'pinterest' => $coworker->get('pinterest'),
+                    'googleplus' => $coworker->get('googleplus'),
+                    'skype' => $coworker->get('skype'),
+                );
+                if (array_filter($social_links)): ?>
+                    <ul>
+                        <?php foreach($social_links as $social_network => $social_link): ?>
+                            <?php if (!empty($social_link)): ?>
+                                <li>
+                                    <a href="<?= $social_link; ?>">
+                                        <img src="<?php bloginfo('template_directory'); ?>/images/social-<?= $social_network; ?>.jpg" alt="<?= $social_network; ?>" />
+                                    </a>
+                                </li>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
             </div>
             <div class="coworker-tags">
                 <?php foreach ($coworker->getTags() as $tag): ?>
