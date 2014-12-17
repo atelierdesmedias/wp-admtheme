1c8f15b37fc6324e2bef6f6efc68a08f89b50389
PaulineGam <paulinegamore@gmail.com>
2014-12-17 11:51:45 +0100

    add sidebar/nav

--- category.php
+++ category.php
@@ -1,8 +1,8 @@
 <?php
-  
+
 
 get_header(); ?>
-
+<section id="content" role="region" class="default-content mod left w70">
     <header class="page-header">
         <h1 class="entry-title"><?php
 						printf( __( 'Catégorie: %s', 'themename' ), '<span>' . single_cat_title( '', false ) . '</span>' );
@@ -14,8 +14,14 @@ get_header(); ?>
     <div class="blog">
       <?php get_template_part( 'loop', 'category' ); ?>
     </div>
+    <div class="nav-single">
+<span class="nav-previous"><?php previous_posts_link( '&laquo; Articles précédents' ); ?></span>
+<span class="nav-next"><?php next_posts_link( 'Articles suivants &raquo;', '' ); ?></span>
+</div>
 
 </section>
 
-<?php //get_sidebar(); ?>
+<div class="sidebar-blog">
+<?php get_sidebar( $name ); ?>
+</div>
 <?php get_footer(); ?>
