df6062401c00ab7adf88d3f432fff653b6613f66
PaulineGam <paulinegamore@gmail.com>
2014-12-18 12:59:10 +0100

    change script analytics

--- footer.php
+++ footer.php
@@ -33,17 +33,14 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 
 </body>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['var_setAccount', 'UA-22457099-1']);
-  _gaq.push(['_trackPageview']);
-  (function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/typejavascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-  }parentNode)();
-</script>script
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-22457099-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </html>
