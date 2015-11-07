7ba8afeb2d49cba0699ba55bd105d156ed36a7a6
Thibault Clauzier <tclauzier@gmail.com>
2015-11-07 15:59:54 +0100

    footer update (markup fix + responsive)

--- footer.php
+++ footer.php
@@ -1,34 +1,27 @@
 <?php 
 /**
  * footer.php
+ *
  */
 ?>
     </div><!-- #main  -->
-
-    <footer role="contentinfo">
-        <div class="widget-area">
-            <?php if ( is_active_sidebar( 'footer-widget-area' ) ){
-                dynamic_sidebar( 'footer-widget-area' );
-            } ?>
-        </div>
-
-        <div class="copyright" style=" background-color:white;">
-            <table>
-                <tr>
-                    <td><img src="<?php echo get_template_directory_uri(); ?>/images/logo-footer.png"></img></td>
-                    <td id="footer-text"><p>9 quai André Lassagne - 69001 Lyon</p><p style="color:#848484">Tel : 09 72 33 20 92</p></td>
-                    <td id="footer-links">
-                        <a href="<?php echo home_url('/credits'); ?>">Crédits</a> - 
-                        <a href="<?php echo home_url('/mentions-legales'); ?>">Mentions légales</a> -
-                        <a href="<?php echo home_url('/nos-partenaires'); ?>">Nos partenaires</a> -
-                        <a href="<?php echo home_url('/espace-presse'); ?>">Espace presse</a>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    </footer>
-
 </div><!-- .container -->
+
+<div id="page-footer-container">
+    <footer id="page-footer" class="container" role="contentinfo">
+        <div id="page-footer-information">
+            <img id="page-footer-logo" src="<?php echo get_template_directory_uri(); ?>/images/logo-footer.png" />
+            <span id="page-footer-adress">9 quai André Lassagne - 69001 Lyon</span>
+            <span id="page-footer-phone">Tel : 09 72 33 20 92</span>
+        </div>
+        <nav id="page-footer-links">
+            <a href="<?php echo home_url('/credits'); ?>">Crédits</a> -
+            <a href="<?php echo home_url('/mentions-legales'); ?>">Mentions légales</a> -
+            <a href="<?php echo home_url('/nos-partenaires'); ?>">Nos partenaires</a> -
+            <a href="<?php echo home_url('/espace-presse'); ?>">Espace presse</a>
+        </nav>
+    </footer>
+</div>
 
 <?php wp_footer(); ?>
 
