e929e9ec2e31218991a4e1f91bd8cf42301b7ebd
Thibault Clauzier <tclauzier@gmail.com>
2015-11-10 11:37:13 +0100

    sidebar formules-page update

---
+++ formules-page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Formule Page
+ * Description: A template with the special formule page
+ *
+ *
+ */
+
+get_header(); ?>
+<?php the_post(); ?>
+
+<section id="content" role="region" class="default-content mod left w70">
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
+
+        <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </header><!-- .entry-header -->
+
+        <div class="formule-content">
+            <?php the_content(); ?>
+            <?php wp_link_pages( 'before=<div class="page-link">' . __( 'Pages:', 'themename' ) . '&after=</div>' ); ?>
+        </div><!-- .entry-content -->
+
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php // comments_template( '', true ); ?>
+
+</section>
+
+<div class="sidebar-container" id="page-sidebar-container">
+    <?= array_shift(get_post_meta( get_the_ID() , 'sidebar' )); ?>
+</div>
+
+<?php get_footer(); ?>
