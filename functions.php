ab1090da37dfa34afe56e8afcd25a9a74ab9ebef
Willy Brauner <mail@willybrauner.com>
2018-05-23 13:50:21 +0200

    refacto php component to twig

--- functions.php
+++ functions.php
@@ -3,8 +3,6 @@
 //require_once( __DIR__ . '/vendor/autoload.php' );
 //$timber = new Timber\Timber();
 
-
-
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
